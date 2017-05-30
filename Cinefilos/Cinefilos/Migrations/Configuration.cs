8b2b2d5871e7b6d1d3745127e7124d9b6f65ba4c
Jean Schuchardt <jeanschuchardt@hotmail.com>
2017-05-30 17:53:20 -0300

    correção banco de dados

--- Cinefilos/Cinefilos/Migrations/Configuration.cs
+++ Cinefilos/Cinefilos/Migrations/Configuration.cs
@@ -1,9 +1,13 @@
 namespace Cinefilos.Migrations
 {
+    using Microsoft.AspNet.Identity;
     using System;
     using System.Data.Entity;
     using System.Data.Entity.Migrations;
     using System.Linq;
+    using Models;
+    using System.Web.Security;
+    using Microsoft.AspNet.Identity.EntityFramework;
 
     internal sealed class Configuration : DbMigrationsConfiguration<Cinefilos.Models.ApplicationDbContext>
     {
@@ -14,19 +18,99 @@ namespace Cinefilos.Migrations
 
         protected override void Seed(Cinefilos.Models.ApplicationDbContext context)
         {
-            //  This method will be called after migrating to the latest version.
+            context.Roles.AddOrUpdate(r => r.Name,
+             new IdentityRole
+             {
+                 Name = "Administrador"
+             }
+         );
+            context.Roles.AddOrUpdate(r => r.Name,
+               new IdentityRole
+               {
+                   Name = "Atendente"
+               }
+           );
 
-            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
-            //  to avoid creating duplicate seed data. E.g.
-            //
-            //    context.People.AddOrUpdate(
-            //      p => p.FullName,
-            //      new Person { FullName = "Andrew Peters" },
-            //      new Person { FullName = "Brice Lambson" },
-            //      new Person { FullName = "Rowan Miller" }
-            //    );
+            context.Roles.AddOrUpdate(r => r.Name,
+               new IdentityRole
+               {
+                   Name = "Cliente"
+               }
+           );
+            context.Roles.AddOrUpdate(r => r.Name,
+              new IdentityRole
+              {
+                  Name = "Gerente"
+              }
+          );
+           
 
-           // context.Roles.AddOrUpdate            //
+            var hasher = new PasswordHasher();
+            context.Users.AddOrUpdate(p => p.Email,
+                new ApplicationUser
+                {
+                    Email = "admin@cinema.com",
+                    UserName = "admin@cinema.com",
+                    PasswordHash = hasher.HashPassword("Pass@word1"),
+                    LockoutEnabled = true,
+                });
+            context.Users.AddOrUpdate(p => p.Email,
+               new ApplicationUser
+               {
+                   Email = "lucifer@cinema.com",
+                   UserName = "lucifer@cinema.com",
+                   PasswordHash = hasher.HashPassword("Pass@word1"),
+                   LockoutEnabled = true,
+               });
+            context.Users.AddOrUpdate(p => p.Email,
+               new ApplicationUser
+               {
+                   Email = "odin@cinema.com",
+                   UserName = "odin@cinema.com",
+                   PasswordHash = hasher.HashPassword("Pass@word1"),
+                   LockoutEnabled = true,
+
+
+               });
+
+            context.Users.AddOrUpdate(p => p.Email,
+               new ApplicationUser
+               {
+                   Email = "orfeu@cinema.com",
+                   UserName = "orfeu@cinema.com",
+                   PasswordHash = hasher.HashPassword("Pass@word1"),
+                   LockoutEnabled = true,
+               });
+
+
+            ApplicationUser user = context.Users.Where(u => u.UserName.Equals("admin@cinema.com", StringComparison.CurrentCultureIgnoreCase)).FirstOrDefault();
+            var UserManager = new UserManager<ApplicationUser>(new UserStore<ApplicationUser>(context));
+            UserManager.AddToRole(user.Id, "Administrador");
+            UserManager.UpdateSecurityStamp(user.Id);
+            user.LockoutEnabled = true;
+
+
+            user = context.Users.Where(u => u.UserName.Equals("lucifer@cinema.com", StringComparison.CurrentCultureIgnoreCase)).FirstOrDefault();
+            UserManager = new UserManager<ApplicationUser>(new UserStore<ApplicationUser>(context));
+            UserManager.AddToRole(user.Id, "Gerente");
+            UserManager.UpdateSecurityStamp(user.Id);
+            user.LockoutEnabled = true;
+
+            user = context.Users.Where(u => u.UserName.Equals("odin@cinema.com", StringComparison.CurrentCultureIgnoreCase)).FirstOrDefault();
+            UserManager = new UserManager<ApplicationUser>(new UserStore<ApplicationUser>(context));
+            UserManager.AddToRole(user.Id, "Atendente");
+            UserManager.UpdateSecurityStamp(user.Id);
+            user.LockoutEnabled = true;
+
+            user = context.Users.Where(u => u.UserName.Equals("orfeu@cinema.com", StringComparison.CurrentCultureIgnoreCase)).FirstOrDefault();
+            UserManager = new UserManager<ApplicationUser>(new UserStore<ApplicationUser>(context));
+            UserManager.AddToRole(user.Id, "Cliente");
+            UserManager.UpdateSecurityStamp(user.Id);
+            user.LockoutEnabled = true;
+
+
+
+
         }
     }
 }
