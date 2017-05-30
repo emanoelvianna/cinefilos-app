613a6aaa167810607b9deb5262266c15a03c60fb
Jean Schuchardt <jeanschuchardt@hotmail.com>
2017-05-30 18:15:06 -0300

    Revert "merge"
    This reverts commit ff39583fa1e499e7c51e49879b5775b584684d67.

--- Cinefilos/Cinefilos/Migrations/Configuration.cs
+++ Cinefilos/Cinefilos/Migrations/Configuration.cs
@@ -65,7 +65,7 @@ namespace Cinefilos.Migrations
                     },
                     new Filmes
                     {
-                        Title = "Ghostbusters 2",
+                        Title  = "Ghostbusters 2",
                         ReleaseDate = DateTime.Parse("1986-2-23"),
                         Director = "Ivan Reitman",
                         Duracao = 107,
@@ -80,56 +80,54 @@ namespace Cinefilos.Migrations
                         Duracao = 90,
                         Id_genero = 26,
 
-                    }
+            },
 
-            //new Filmes
-            //{
-            //    Title = "Star Wars - O Despertar da Força",
-            //    ReleaseDate = DateTime.Parse("2015-12-17"),
-            //    Director = "J. J. Abrams",
-            //    Id_genero = 8,
+            new Filmes
+            {
+                Title = "Star Wars - O Despertar da Força",
+                ReleaseDate = DateTime.Parse("2015-12-17"),
+                Director = "J. J. Abrams",
+                Id_genero = 8,
 
-            //},
+            },
 
-            //new Filmes
-            //{
-            //    Title = "Interstellar",
-            //    ReleaseDate = DateTime.Parse("2014-1-6"),
-            //    Director = "Christopher Nolan",
-            //    Id_genero = 8,
+            new Filmes
+            {
+                Title = "Interstellar",
+                ReleaseDate = DateTime.Parse("2014-1-6"),
+                Director = "Christopher Nolan",
+                Id_genero = 8,
 
-            //},
+            },
 
-            //new Filmes
-            //{
-            //    Title = "Spectre",
-            //    ReleaseDate = DateTime.Parse("2015-11-5"),
-            //    Director = "Sam Mendes",
-            //    Id_genero = 1,
+            new Filmes
+            {
+                Title = "Spectre",
+                ReleaseDate = DateTime.Parse("2015-11-5"),
+                Director = "Sam Mendes",
+                Id_genero = 1,
 
-            //}
-            );
+            }
 
-#region
-            //new Filmes {
-            //    Title = "The Shawshank Redemption",
-            //    ReleaseDate = DateTime.Parse("10/14/1994"),
-            //    Director = "Frank Darabont",
-            //    Id = context.Generos.Single(g => g.Name == "Drama").Id,
+         //new Filmes {
+         //    Title = "The Shawshank Redemption",
+         //    ReleaseDate = DateTime.Parse("10/14/1994"),
+         //    Director = "Frank Darabont",
+         //    Id = context.Generos.Single(g => g.Name == "Drama").Id,
 
-            //},
-            //new Filmes { Title = "The Godfather", Director = "Francis Ford Coppola", ReleaseDate = DateTime.Parse("3/24/1972"), Id = context.Generos.Single(g => g.Name == "Drama").Id },
-            //new Filmes { Title = "Pulp Fiction", Director = "Quentin Tarantino", ReleaseDate = DateTime.Parse("10/14/1994"), Id = context.Generos.Single(g => g.Name == "Thriller").Id },
-            //new Filmes { Title = "Schindlers List", Director = "Steven Spielberg", ReleaseDate = DateTime.Parse("2/4/1994"), Id = context.Generos.Single(g => g.Name == "Drama").Id },
-            //new Filmes { Title = "The Dark Knight", Director = "Christopher Nolan", ReleaseDate = DateTime.Parse("7/18/2008"), Id = context.Generos.Single(g => g.Name == "Action").Id },
-            //new Filmes { Title = "The Lord of the Rings: The Return of the King", Director = "Peter Jackson", ReleaseDate = DateTime.Parse("12/17/2003"), Id = context.Generos.Single(g => g.Name == "Action").Id },
-            //new Filmes { Title = "Star Wars", Director = "George Lucas", ReleaseDate = DateTime.Parse("5/25/1977"), Id = context.Generos.Single(g => g.Name == "Action").Id },
-            //new Filmes { Title = "The Matrix", Director = "The Wachowski Brothers", ReleaseDate = DateTime.Parse("3/31/1999"), Id = context.Generos.Single(g => g.Name == "Sci-Fi").Id },
-            //new Filmes { Title = "Forrest Gump", Director = "Robert Zemeckis", ReleaseDate = DateTime.Parse("7/6/1994"), Id = context.Generos.Single(g => g.Name == "Comedy").Id },
-            //new Filmes { Title = "Monty Python and the Holy Grail", Director = " Terry Gilliam & Terry Jones", ReleaseDate = DateTime.Parse("5/23/1975"), Id = context.Generos.Single(g => g.Name == "Comedy").Id },
-            //new Filmes { Title = "2001: A Space Odyssey", Director = "Stanley Kubrick", ReleaseDate = DateTime.Parse("4/29/1968"), Id = context.Generos.Single(g => g.Name == "Sci-Fi").Id },
-            //new Filmes { Title = "Back to the Future", Director = "Robert Zemeckis", ReleaseDate = DateTime.Parse("1/22/1989"), Id = context.Generos.Single(g => g.Name == "Family").Id },
-            //new Filmes { Title = "Monsters Inc", Director = "Pete Docter & David Silverman", ReleaseDate = DateTime.Parse("11/2/2001"), Id = context.Generos.Single(g => g.Name == "Family").Id }
+         //},
+         //new Filmes { Title = "The Godfather", Director = "Francis Ford Coppola", ReleaseDate = DateTime.Parse("3/24/1972"), Id = context.Generos.Single(g => g.Name == "Drama").Id },
+         //new Filmes { Title = "Pulp Fiction", Director = "Quentin Tarantino", ReleaseDate = DateTime.Parse("10/14/1994"), Id = context.Generos.Single(g => g.Name == "Thriller").Id },
+         //new Filmes { Title = "Schindlers List", Director = "Steven Spielberg", ReleaseDate = DateTime.Parse("2/4/1994"), Id = context.Generos.Single(g => g.Name == "Drama").Id },
+         //new Filmes { Title = "The Dark Knight", Director = "Christopher Nolan", ReleaseDate = DateTime.Parse("7/18/2008"), Id = context.Generos.Single(g => g.Name == "Action").Id },
+         //new Filmes { Title = "The Lord of the Rings: The Return of the King", Director = "Peter Jackson", ReleaseDate = DateTime.Parse("12/17/2003"), Id = context.Generos.Single(g => g.Name == "Action").Id },
+         //new Filmes { Title = "Star Wars", Director = "George Lucas", ReleaseDate = DateTime.Parse("5/25/1977"), Id = context.Generos.Single(g => g.Name == "Action").Id },
+         //new Filmes { Title = "The Matrix", Director = "The Wachowski Brothers", ReleaseDate = DateTime.Parse("3/31/1999"), Id = context.Generos.Single(g => g.Name == "Sci-Fi").Id },
+         //new Filmes { Title = "Forrest Gump", Director = "Robert Zemeckis", ReleaseDate = DateTime.Parse("7/6/1994"), Id = context.Generos.Single(g => g.Name == "Comedy").Id },
+         //new Filmes { Title = "Monty Python and the Holy Grail", Director = " Terry Gilliam & Terry Jones", ReleaseDate = DateTime.Parse("5/23/1975"), Id = context.Generos.Single(g => g.Name == "Comedy").Id },
+         //new Filmes { Title = "2001: A Space Odyssey", Director = "Stanley Kubrick", ReleaseDate = DateTime.Parse("4/29/1968"), Id = context.Generos.Single(g => g.Name == "Sci-Fi").Id },
+         //new Filmes { Title = "Back to the Future", Director = "Robert Zemeckis", ReleaseDate = DateTime.Parse("1/22/1989"), Id = context.Generos.Single(g => g.Name == "Family").Id },
+         //new Filmes { Title = "Monsters Inc", Director = "Pete Docter & David Silverman", ReleaseDate = DateTime.Parse("11/2/2001"), Id = context.Generos.Single(g => g.Name == "Family").Id }
 
 
             //);
@@ -150,7 +148,7 @@ namespace Cinefilos.Migrations
             //    );
             //
 
-#endregion
+            #endregion
 
 
         }///
